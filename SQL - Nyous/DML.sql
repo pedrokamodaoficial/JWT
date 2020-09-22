3d303f6da180c11d0be6d0609c344cf5ac9b5bbb
Pedro Kamoda <pedrokamoda04@gmail.com>
2020-09-22 14:56:39 -0300

    estrutura atualizada e arrumada

---
+++ SQL - Nyous/DML.sql
@@ -0,0 +1,22 @@
+USE Nyous;
+GO
+
+INSERT INTO Acesso (Tipo) VALUES 
+	('Administrador'),
+	('Padrao');
+
+INSERT INTO Categoria (Titulo) VALUES 
+	('Meetup'),
+	('Workshop'),
+	('Feira De Tecnologia'),
+	('Live');
+
+INSERT INTO Localizacao (Logradouro, Numero, Complemento, Bairro, Cidade, UF, CEP)
+	VALUES
+	('Alameda Barao de Limeira', 539, NULL, 'Santa Cecilia', 'Sao Paulo', 'SP', '01202-001');
+
+INSERT INTO Usuario (Nome, Email, Senha, DataNascimento, IdAcesso) VALUES
+	('Paulo Brandao', 'paulo@senai.com.br', '1234567890', '1970-04-02T00:00:00', 1);
+
+INSERT INTO Evento (DataEvento, Capacidade, IdLocalizacao, IdCategoria) VALUES
+	('2020-09-25T22:00:00', 100, 1, 1);
---
+++ SQL - Nyous/DQL.sql
@@ -0,0 +1,16 @@
+USE Nyous;
+
+SELECT * FROM Acesso;
+
+SELECT * FROM Categoria;
+
+SELECT * FROM Usuario;
+
+SELECT Evento.*, Categoria.Titulo AS NomeCategoria, Localizacao.* FROM Evento
+	INNER JOIN Categoria ON Evento.IdCategoria = Categoria.IdCategoria
+	INNER JOIN Localizacao ON Evento.IdLocalizacao = Localizacao.IdLocalizacao
+;
+
+SELECT *
+	FROM Usuario
+	INNER JOIN Acesso on Usuario.IdAcesso = Acesso.IdAcesso;
